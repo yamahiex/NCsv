568828a272dd93389a83e6a8d08f1c7aefc9ba0d
YAMAMOTO Hiroyasu <yamahi.ex@gmail.com>
2020-05-03 19:25:11 +0900

    refactor: Return Task without await
    It is more appropriate to return Task without await, so I changed it.

--- src/NCsv/NCsv/CsvSerializer.cs
+++ src/NCsv/NCsv/CsvSerializer.cs
@@ -27,9 +27,9 @@ namespace NCsv
         /// </summary>
         /// <param name="objects">オブジェクト。</param>
         /// <returns>CSV文字列。</returns>
-        public async Task<string> SerializeAsync(params T[] objects)
+        public Task<string> SerializeAsync(params T[] objects)
         {
-            return await Task.Run(() => Serialize(objects));
+            return Task.Run(() => Serialize(objects));
         }
 
         /// <summary>
@@ -50,9 +50,9 @@ namespace NCsv
         /// </summary>
         /// <param name="writer">CSVを書き込む<see cref="TextWriter"/>。</param>
         /// <param name="objects">オブジェクト。</param>
-        public async Task SerializeAsync(TextWriter writer, params T[] objects)
+        public Task SerializeAsync(TextWriter writer, params T[] objects)
         {
-            await Task.Run(() => Serialize(writer, objects));
+            return Task.Run(() => Serialize(writer, objects));
         }
 
         /// <summary>
@@ -79,9 +79,9 @@ namespace NCsv
         /// <param name="csv">CSV文字列。</param>
         /// <returns>オブジェクト。</returns>
         /// <exception cref="CsvDeserializeException">逆シリアル化時にエラーが発生した場合にスローされます。</exception>
-        public async Task<List<T>> DeserializeAsync(string csv)
+        public Task<List<T>> DeserializeAsync(string csv)
         {
-            return await Task.Run(() => Deserialize(csv));
+            return Task.Run(() => Deserialize(csv));
         }
 
         /// <summary>
@@ -101,9 +101,9 @@ namespace NCsv
         /// <param name="reader">CSVを読み取る<see cref="TextReader"/>。</param>
         /// <returns>オブジェクト。</returns>
         /// <exception cref="CsvDeserializeException">逆シリアル化時にエラーが発生した場合にスローされます。</exception>
-        public async Task<List<T>> DeserializeAsync(TextReader reader)
+        public Task<List<T>> DeserializeAsync(TextReader reader)
         {
-            return await Task.Run(() => Deserialize(reader));
+            return Task.Run(() => Deserialize(reader));
         }
 
         /// <summary>
@@ -123,9 +123,9 @@ namespace NCsv
         /// <param name="parser">CSVを解析する<see cref="CsvTextFieldParser"/>。</param>
         /// <returns>オブジェクト。</returns>
         /// <exception cref="CsvDeserializeException">逆シリアル化時にエラーが発生した場合にスローされます。</exception>
-        public async Task<List<T>> DeserializeAsync(CsvTextFieldParser parser)
+        public Task<List<T>> DeserializeAsync(CsvTextFieldParser parser)
         {
-            return await Task.Run(() => Deserialize(parser));
+            return Task.Run(() => Deserialize(parser));
         }
 
         /// <summary>
